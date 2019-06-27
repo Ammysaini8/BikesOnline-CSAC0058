3c5b53491da6d658d4a72430f8aaa74f7d271592
Ammysaini8 <51136609+Ammysaini8@users.noreply.github.com>
2019-06-27 15:18:50 -0400

    Add files via upload

--- AddBrand.aspx.cs
+++ AddBrand.aspx.cs
@@ -14,7 +14,7 @@ namespace Home
         public string cnstring = "Data Source=amrinder\\sqlexpress;Initial Catalog=Project;Integrated Security=True";
         protected void Page_Load(object sender, EventArgs e)
         {
-            String myquery = "Select *from Brand";
+            String myquery = "Select *from Brand1";
             SqlConnection con = new SqlConnection(cnstring);
             SqlCommand cmd = new SqlCommand();
             cmd.CommandText = myquery;
@@ -29,7 +29,36 @@ namespace Home
 
         protected void Button1_Click(object sender, EventArgs e)
         {
+            SqlConnection con = new SqlConnection(cnstring);
+            con.Open();
+            if (con.State == System.Data.ConnectionState.Open)
+            {
+                string a = "insert into Brand1(Brand_name,Brand_ID)values('"
+                    + TextBox1.Text.ToString() + "','" + TextBox2.Text.ToString() + "')";
+                SqlCommand comd = new SqlCommand(a, con);
+                comd.ExecuteNonQuery();
+                String myquery = "Select *from Brand1";
+                SqlConnection conn = new SqlConnection(cnstring);
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandText = myquery;
+                cmd.Connection = conn;
+                SqlDataAdapter da = new SqlDataAdapter();
+                da.SelectCommand = cmd;
+                DataSet set = new DataSet();
+                da.Fill(set);
+                GridView1.DataSource = set;
+                GridView1.DataBind();
 
+            }
+
+
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+        
+ 
         }
     }
 }
